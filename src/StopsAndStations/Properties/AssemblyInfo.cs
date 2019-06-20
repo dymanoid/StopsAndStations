68cfbf39d1825f5f845e2d663d1b4b213559f95a
dymanoid <9433345+dymanoid@users.noreply.github.com>
2019-06-20 15:09:08 +0200

    Create solution

---
+++ src/StopsAndStations/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="AssemblyInfo.cs" company="dymanoid">
+// Copyright (c) dymanoid. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StopsAndStations")]
+[assembly: AssemblyDescription("A mod for Cities: Skylines")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dymanoid")]
+[assembly: AssemblyProduct("StopsAndStations")]
+[assembly: AssemblyCopyright("Copyright © 2019 dymanoid")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
